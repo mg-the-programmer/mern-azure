5ad3c0ce1931add4804d4de9c8b46b4d167e4023
Manigandan C <manigandan.c2003@gmail.com>
2023-11-07 14:37:58 +0530

    Using Static Build

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.post("/create", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+app.use(express.static("./client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
